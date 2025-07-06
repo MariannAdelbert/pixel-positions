acc17d383ee22621dbd5a4b17a813cf59a51a9b9
Mariann Adelbert <mariannadelbert@gmail.com>
2025-07-06 15:52:44 +0300

    Day 29: Jobs, Tags, TDD, Oh My!

---
+++ tests/Pest.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class JobTest extends TestCase
+{
+    use RefreshDatabase; // See tagab, et iga testi alguses on migratsioonid tehtud
+
+    // ...
+}
